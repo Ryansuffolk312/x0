944137016d441e23f8c501204c866d3250098bfe
Ryansuffolk312 <superrush312@yahoo.com>
2015-09-08 17:08:16 -0400

    Update circles1.java
    turned movement of circles into an UFO floating in the sky

--- circles1.java
+++ circles1.java
@@ -5,18 +5,26 @@ float x,y;
 
 // Setup:  screen size, initialization. //
 void setup() {
-  size(500, 500);        // Try changing the width & height.
-  x=  width/2;
-  y=  height/2;
+  size(500, 500);        
+  x=  width/4;
+  y=  height/3;
+  
 }
 
 // Next frame:  draw three circles. //
 void draw() {
-  ellipse(x, y, 80, 80);
-  ellipse(x+60, y+60, 40, 40);
-  ellipse(x+90, y+90, 20, 20);
- 
-  // Move them???? //--              x=  x + 2;  y=  y + 1;
-  // Insert BREAK here ^
-  // to uncomment the above code,
+   background(44,235, 240);         // added to smooth out animation   
+   // moved, resized, and added shapes to turn into UFO floating in the sky
+ellipse(x, y, 200, 25 );                      
+ fill(12, 75, 108);     
+  ellipse(x+0, y+-40, 125, 75);
+  fill(255);
+  ellipse(x+90, y+90, 200, 50);  
+  fill(255);
+   ellipse(x+-150, y+-200, 200, 50); // added to make another cloud 
+   fill(176);
+        x=  x + 2;  y=  y + 1;
+        
+        
+  
 }
