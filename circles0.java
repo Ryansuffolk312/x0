10e272ea69761c384a1b237d826a26cc1994225c
Ryansuffolk312 <superrush312@yahoo.com>
2015-09-08 16:02:04 -0400

    Update circles0.java
    Made screen bigger and wider and moved circle more towards the center of the screen near the bottom. Turned circle into robot. YOU WILL ALL BE ASSIMILATED!!!!!

--- circles0.java
+++ circles0.java
@@ -2,10 +2,21 @@
 
 // Setup:  screen size. //
 void setup() {
-  size(200, 200);                // Try changing the width & height.
+  size(400, 350);                // Made screen bigger and wider
 }
-
+               
 // Next frame:  circle at (x,y). //
 void draw() {
-  ellipse( 50,50, 30,30 );       // Try changing the (x,y) and size.
+  ellipse( 200, 225, 150,200 );       // increased circle's size, moved it towards center
+
+//decided to give it a face
+  rect( 150, 175, 25, 50 );           //left eye                
+  
+  rect( 225, 175, 25, 50);           //right eye
+ 
+  rect(150, 250, 100, 25);           // mouth
+  
+  ellipse(200, 345, 125, 90);        // body  
+
+rect(150, 300, 100, 25, 7);         // neck thingy (technical term)
 }
